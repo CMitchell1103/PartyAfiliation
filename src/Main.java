e07802c3fa49ecb8dc9b2536c26bef022c26de5d
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-10-02 18:39:53 -0400

    Final commit and push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String partyAfil = "";
+
+        System.out.println("Enter your party affiliation [R,D,I]: ");
+        partyAfil = in.nextLine();
+
+        //Republican
+        if (partyAfil.equals("R")) {
+            System.out.println("You get a Republican Elephant! ");
+
+        // Democrat
+        } else if (partyAfil.equals("D")) {
+
+            System.out.println("You get a Democrat Donkey! ");
+
+        // Independent
+        } else if (partyAfil.equals("I")) {
+
+            System.out.println("You get an Independent Man! ");
+
+        // Other
+        } else {
+
+            System.out.println("I dont know what party this is: " + partyAfil);
+        }
+        in.close();
+
     }
+
+
+
+
+
 }
